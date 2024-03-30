fc05dc9779ba0ae2bf629ea5e19003ff1b22e1fa
DV.Sang <vansangIT2k3@gmai.com>
2024-03-30 12:05:07 +0700

    done added project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./Context_API&useReducer/App.jsx";
+import App from "./Tic-Tac-Toe/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
